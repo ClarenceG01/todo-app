31e5d22da1b0c9ce3ec53f40ec28c91faac49b6c
ClarenceG01 <gatama001@gmail.com>
2023-05-29 23:12:49 +0300

    deletes unused files, adds add task and delete tasks functionality

---
+++ src/components/todo.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const todos = [
+  {
+    text: "This is a sampe todo",
+    isDone: false,
+  },
+  {
+    text: "This is a sampe todo",
+    isDone: false,
+  },
+  {
+    text: "This is a sampe todo",
+    isDone: false,
+  },
+];
+let [task] = todos;
+let { text, value } = task;
+const Todo = () => {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleTask(e) {
+    e.preventDefault();
+    setTodos((prevTodos) => [...prevTodos, task]);
+    setTask("");
+  }
+  function handleDelete(index) {
+    setTodos((prevTodos) => {
+      const updatedTodos = [...prevTodos];
+      updatedTodos.splice(index, 1);
+      return updatedTodos;
+    });
+  }
+  return (
+    <div className="todos">
+      <form className="form" onSubmit={handleTask}>
+        <input
+          type="text"
+          name=""
+          id=""
+          value={task}
+          placeholder="Enter Task"
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {todos.map((todo, index) => (
+        <div className="single-todo" key={index}>
+          <input type="checkbox" name="" id="" />
+          <h4>{todo}</h4>
+          <button onClick={handleDelete}>
+            <i className="fa-solid fa-trash"></i>
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Todo;
